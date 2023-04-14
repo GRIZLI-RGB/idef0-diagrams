2dc1ef033281a98131fad5bde02e234873cd2a51
Evgeniy Kozlov <ek916569@gmail.com>
2023-04-14 15:34:21 +0500

    hgh

--- src/components/DiagramBlock/index.jsx
+++ src/components/DiagramBlock/index.jsx
@@ -2,90 +2,16 @@ import Xarrow from "react-xarrows";
 import "./DiagramBlock.scss";
 
 export const DiagramBlock = ({ currentDiagram, currentDiagrams, currentLevel, styles }) => {
-    console.log(currentDiagrams[currentLevel])
+    console.log(currentDiagram)
+    const checkNextDiagram = () => {
+        let flag = false;
+        currentDiagrams[currentLevel].map((item) => item.id === currentDiagram.id + 1 ? flag = true : null)
+        return flag;
+    }
     return (
         <div className="diagramBlock" id={`diagramBlock-${currentDiagram?.id}`} style={styles}>
             <h6 className="diagramBlock__name">{currentDiagram?.name}</h6>
             <p className="diagramBlock__id">{currentLevel === "A0" ? "A0" : currentLevel + currentDiagram?.id}</p>
-            {/* {currentDiagram?.inputs?.length > 0 && (
-                <ul className="diagramBlock__ul diagramBlock__inputs">
-                    {currentDiagram.inputs.map((input, index) => (
-                        <>
-                            <li id={`input-${index}`} className={`diagramBlock__ul-li diagramBlock__inputs-input diagramBlock__ul-li-${currentDiagram.id}`}></li>
-                            <Xarrow
-                                start={`input-${index}`}
-                                end={`diagramBlock-${currentDiagram?.id}`}
-                                color="#353535"
-                                strokeWidth={1}
-                                gridBreak="90%"
-                                path="grid"
-                                labels={{start: <span style={{
-                                    fontSize: "10px",
-                                    maxWidth: "80px",
-                                    display: "block",
-                                    transform: "translateX(-105%)"
-                                }}>{input}</span>}}
-                            />
-                        </>
-                    ))}
-                </ul>
-            )}
-            {currentDiagram?.outputs?.length > 0 && (
-                <ul className="diagramBlock__ul diagramBlock__outputs">
-                    {currentDiagram.outputs.map((output, index) => (
-                        <>
-                            <li id={`output-${index}`} className={`diagramBlock__ul-li diagramBlock__outputs-input diagramBlock__ul-li-${currentDiagram.id}`}></li>
-                            <Xarrow
-                                start={`diagramBlock-${currentDiagram?.id}`}
-                                end={`output-${index}`}
-                                color="#353535"
-                                strokeWidth={1}
-                                gridBreak="90%"
-                                path="grid"
-                                labels={{end: <span style={{
-                                    fontSize: "10px",
-                                    maxWidth: "80px",
-                                    display: "block",
-                                    transform: "translateX(105%)"
-                                }}>{output}</span>}}
-                            />
-                        </>
-                    ))}
-                </ul>
-            )}
-            {currentDiagram?.controls?.length > 0 && (
-                <ul className="diagramBlock__ul diagramBlock__controls">
-                    {currentDiagram.controls.map((control, index) => (
-                        <>
-                            <li id={`control-${index}`} className={`diagramBlock__ul-li diagramBlock__controls-input diagramBlock__ul-li-${currentDiagram.id}`}>{control}</li>
-                            <Xarrow
-                                start={`control-${index}`}
-                                end={`diagramBlock-${currentDiagram?.id}`}
-                                color="#353535"
-                                strokeWidth={1}
-                                gridBreak="90%"
-                                path="grid"
-                            />
-                        </>
-                    ))}
-                </ul>
-            )}
-            {currentDiagram?.mechanisms?.length > 0 && (
-                <ul className="diagramBlock__ul diagramBlock__mechanims">
-                    {currentDiagram?.mechanisms?.map((mechanim, index) => (
-                        <>
-                        <li id={`mechanim-${index}`} className={`diagramBlock__ul-li diagramBlock__mechanims-input diagramBlock__ul-li-${currentDiagram.id}`}>{mechanim}</li>
-                        <Xarrow
-                            start={`mechanim-${index}`}
-                            end={`diagramBlock-${currentDiagram?.id}`}
-                            color="#353535"
-                            strokeWidth={1}
-                            gridBreak="90%"
-                            path="grid"
-                        /></>
-                    ))}
-                </ul>
-            )} */}
             {currentDiagram?.inputs?.length > 0 && (
                 <ul className="diagramBlock__ul diagramBlock__inputs">
                     {currentDiagram.inputs.map((input, index) => (
@@ -114,7 +40,22 @@ export const DiagramBlock = ({ currentDiagram, currentDiagrams, currentLevel, st
                     {currentDiagram.outputs.map((output, index) => (
                         <>
                             <li id={`output-${index}-${currentDiagram?.id}`} className={`diagramBlock__ul-li diagramBlock__outputs-input diagramBlock__ul-li-${currentDiagram.id}`}></li>
-                            <Xarrow
+                            {
+                                checkNextDiagram() ? <Xarrow
+                                start={`diagramBlock-${currentDiagram?.id}`}
+                                end={`diagramBlock-${currentDiagram?.id + 1}`}
+                                color="#353535"
+                                strokeWidth={1}
+                                gridBreak="90%"
+                                path="grid"
+                                labels={{start: <span style={{
+                                    fontSize: "10px",
+                                    maxWidth: "80px",
+                                    display: "block",
+                                    padding: "0 0 20px 100px"
+                            }}>{output}</span>}}
+                            /> : (
+                                    <Xarrow
                                 start={`diagramBlock-${currentDiagram?.id}`}
                                 end={`output-${index}-${currentDiagram?.id}`}
                                 color="#353535"
@@ -128,6 +69,8 @@ export const DiagramBlock = ({ currentDiagram, currentDiagrams, currentLevel, st
                                     transform: "translateX(105%)"
                             }}>{output}</span>}}
                             />
+                                )
+                            }
                         </>
                     ))}
                 </ul>
